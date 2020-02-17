dfa36aea53a517735ef476da24f440f0b7513731
Philtherage <phillipwood61@gmail.com>
2020-02-16 20:14:34 -0500

    Section 2: Move Using Nav Mesh Agent.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPoint();
+    }
+
+    private void MoveToPoint()
+    {
+        //Setsup and raycasts to mouse Position then moves player to raycast hit point
+        Vector3 mousepos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Ray ray = Camera.main.ViewportPointToRay(mousepos);
+        RaycastHit hit;
+
+        if (Input.GetButton("Fire1"))
+        {
+            Physics.Raycast(ray, out hit);
+            navMeshAgent.SetDestination(hit.point);
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # RPG_Course
  
 * Intial Commit.
+
+* Section 2: Create A Simple Sandbox.
+
+* Section 2: Move Using Nav Mesh Agent.
